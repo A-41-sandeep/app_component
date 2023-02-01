d0a19444549ff1b6835e259fb53a73ba6fecf2a5
sandeep kumar <sandeep.kumar_cs20@gla.ac.in>
2023-02-01 19:56:40 -0300

    .

---
+++ src/FirstComponet.js
@@ -0,0 +1,12 @@
+//using function
+function FirstComponent()
+{
+    return(
+        <>
+        <h1>This is first component using function</h1>
+        <h2>welcome here</h2>
+        </>
+    );
+}
+
+export default FirstComponent;
---
+++ src/SecondComponent.js
@@ -0,0 +1,17 @@
+//component using class
+
+import React from "react";
+
+class SecondComponent extends React.Component
+{
+    render()
+    {
+        return (
+            <>
+            <h1>Component using class</h1>
+            <h2>Welcome here too</h2>
+            </>
+        );
+    }
+}
+export default SecondComponent;
